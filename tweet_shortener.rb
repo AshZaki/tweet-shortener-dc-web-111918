54274e350e89b3f9da9e8e91ae70380e50c6534c
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-05 20:21:15 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+  {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&"
+  }
+end
+
+def word_substituter(str_tweet)
+  tweet = str_tweet.split(" ").map! do |word|
+    if dictionary.keys.include?(word.downcase)
+      dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  tweet.join(" ")
+end
+
+
+def bulk_tweet_shortener(tweet_arr)
+  tweet_arr.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def 
